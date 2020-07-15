888fbacbfaf2b55905e785b632dc040ca65f7264
Dang Thanh Hao <aszqsc@gmail.com>
2020-07-15 20:45:34 +0700

    add drawer

--- MovieDemo/app/src/main/java/com/example/moviedemo/MainActivity.kt
+++ MovieDemo/app/src/main/java/com/example/moviedemo/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.moviedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.moviedemo.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController = findNavController(R.id.myNavHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+//        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+//            if (nd.id == nc.graph.startDestination) {
+//
+//                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+//            } else {
+//                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+//            }
+//        }
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.myNavHostFragment)
+//        return  navController.navigateUp();
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
 }
